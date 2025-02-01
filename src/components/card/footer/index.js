170405e265cdaf5cb24c2836ca80fb215989d7ab
Mohamad Bakhshaish <118728086+MohamadSD2002@users.noreply.github.com>
2025-02-01 03:34:02 +0330

    Delete src directory

--- src/components/card/footer/index.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="w-full text-center border-t pt-2 text-[#31363f] font-semibold text-[10px]">
-      @mohamadsd2002
-    </div>
-  );
-}
